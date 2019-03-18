79ea21c38b77c55a5a1e282c555a43ab1f8d89e5
Yang <guanhui07@gmail.com>
2019-03-18 14:39:24 +0800

    update

--- resources/views/content.blade.php
+++ resources/views/content.blade.php
@@ -28,9 +28,8 @@
                         {{ $item['text'] }}
                     </a>
                 </li>
-                @endif
+              @endif
             @endforeach
-
         </ol>
         @endif
         <!-- breadcrumb end -->
@@ -47,36 +46,111 @@
 
     </section>
 
-	<div class="adminPopup">
-        <img src="/static/images/indextchide.png" class="adminPopup-close">
-        <input id="l_admin_uid" name="" type="hidden" value="{{Admin::user()->id}}" >
+    {{--  --}}
+    <input id="l_admin_uid" name="" type="hidden" value="{{Admin::user()->id}}" >
+    <input id="l_admin_type" name="" type="hidden" value="{{Admin::user()->type}}" >
+    {{-- 资源池弹框 --}}
+    <div class="adminPopup" id="adminPopup">
+        <img src="/houtai/images/adminPopup_close.png" class="adminPopup-close" id="adminPopup-close">
         <div class="adminPopup-con">
+            <img src="/houtai/images/adminPopup_bg.png" class="adminPopup_bg">
             <h4>推瓜网提醒你</h4>
             <p>公共池资源请尽快领取，</br>八分钟后将重新分配！</p>
             <div class="adminPopup-conlist">
-                <a>前往领取</a>
+                <a id="adminPopup-type">前往领取</a>
+            </div>
+        </div>
+    </div>
+    {{-- 新求购弹框 --}}
+    <div class="adminPopup" id="adminPopup-buy">
+        <img src="/houtai/images/adminPopup_close.png" class="adminPopup-close" id="adminPopup-buy-close">
+        <div class="adminPopup-con">
+            <img src="/houtai/images/adminPopup_bg.png" class="adminPopup_bg">
+            <h4>推瓜网提醒你</h4>
+            <p>店铺新求购~请尽快回复</p>
+            <div class="adminPopup-conlist">
+                <a id="adminPopup-buy-type">跳转回复</a>
             </div>
         </div>
     </div>
     <script type="text/javascript">
+        var popupTime;
+        var popupBuyTime;
         $(document).ready(function(){
+            var adminPopupId = localStorage.getItem('adminPopupId');
+            var l_admin_type = $('#l_admin_type').val();
+            if (!adminPopupId) {
+                popupShow();
+                window.clearInterval(popupTime);
+                popupTime = setInterval(function(){
+                    popupShow();
+                }, 30000);
+            }
+            // 新求购访问
+            // if (window.location.href.indexOf('/admin/enterprise/requirement/qqbuy') == -1) {
+            //     if (l_admin_type == 1) {
+            //         successBuy();
+            //         window.clearInterval(popupBuyTime);
+            //         popupBuyTime = setInterval(function(){
+            //             successBuy();
+            //         }, 40000);
+            //     }
+            // }
+        });
+        function popupShow(){
+            var uid = $('#l_admin_uid').val();
             $.ajax({
                 type: 'GET',
-                url: '/admin/api/remindAdmin',
+                url: 'http://adminapi.tuigua.com/index/index?uid=' + uid, // '/api2/get1',
+                //url: 'http://121.43.162.244/index/index?uid=' + uid, //'/admin/api/remindAdmin',
                 dataType: 'json',
                 success: function(reponse){
                     if (reponse.code === 200) {
-                        $('.adminPopup').slideDown('slow');
+                        if (reponse.data != -1) {
+                            $('#adminPopup').slideDown('slow');
+                            $('#adminPopup-type').attr('href', '/admin/enterprise/resoucepool?type='+ reponse.data +'&no_get=1');
+                        }
+                    } else {
+                        window.clearInterval(popupTime);
+                        localStorage.setItem('adminPopupId', reponse.data.uid);
                     }
                 }
-            })
-        });
+            });
+        }
+        // 求购弹窗
+        function successBuy () {
+            var adminId = $('#l_admin_uid').val();
+            $.ajax({
+                type: 'GET',
+                url: '/trade_new/index.php?r=index/index&adminId=' + adminId,
+                dataType: "json",
+                success: function(reponse){
+                    console.log('reponse=', reponse);
+                    if (reponse.code === 200) {
+                        if (reponse.data != -1) {
+                            $('#adminPopup-buy').slideDown('slow');
+                            $('#adminPopup-buy-type').attr('href', '/admin/enterprise/requirement/qqbuy?shop_buy_id='+ reponse.data.shop_buy_id);
+                        }
+                    } else {
+                        window.clearInterval(popupBuyTime);
+                    }
+                }
+            });
+        }
         $(function () {
-            $('.adminPopup-close').click(function () {
-                $('.adminPopup').slideUp('slow');
-                setTimeout( function(){
-                    $('.adminPopup').slideDown('slow');
-                }, 10000);
+            $('#adminPopup-close').click(function () {
+                $('#adminPopup').slideUp('slow');
+                window.clearInterval(popupTime);
+                popupTime = setInterval(function(){
+                    popupShow();
+                }, 30000);
+            });
+            $('#adminPopup-buy-close').click(function () {
+                $('#adminPopup-buy').slideUp('slow');
+                window.clearInterval(popupBuyTime);
+                popupBuyTime = setInterval(function(){
+                    successBuy();
+                }, 40000);
             });
         });
     </script>
